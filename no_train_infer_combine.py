77a20585c7b3b6a9cb29f653b8beebf0d71f05f0
daewoo myoung <ttagu99@gmail.com>
2019-02-24 12:10:18 +0900

    last submit

--- no_train_infer_combine.py
+++ no_train_infer_combine.py
@@ -29,7 +29,7 @@ from keras.applications.nasnet import NASNetLarge
 from keras.applications.mobilenetv2 import MobileNetV2
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from classification_models.resnet import ResNet18, SEResNet18
-from classification_models.senet import SEResNeXt50,SEResNeXt101
+from classification_models.senet import SEResNeXt50
 from keras.models import Model,load_model
 from keras.optimizers import Adam, SGD
 from keras import Model, Input
@@ -46,10 +46,8 @@ import tensorflow as tf
 from keras.losses import categorical_crossentropy
 from utils import *
 import pickle
-from sklearn.decomposition import PCA
-import gc
-import psutil
-
+pca_w = './Wpca.npy'
+pca_xm = './Xmpca.npy'
 
 def bind_model(model):
     def save(dir_name):
@@ -63,24 +61,17 @@ def bind_model(model):
 
     def infer(queries, _):
         test_path = DATASET_PATH + '/test/test_data'
+
         db = [os.path.join(test_path, 'reference', path) for path in os.listdir(os.path.join(test_path, 'reference'))]
+
         queries = [v.split('/')[-1].split('.')[0] for v in queries]
         db = [v.split('/')[-1].split('.')[0] for v in db]
         queries.sort()
         db.sort()
 
-        num_classes = config.num_classes
-        intermediate_layer_model_small = Model(inputs=model.layers[0].input, outputs=[model.layers[-9].input, model.layers[-9].output])
-        intermediate_layer_model_big = Model(inputs=model.layers[1].input, outputs=[model.layers[-8].input, model.layers[-8].output])
+        queries, query_vecs, references, reference_vecs = get_feature(model, queries, db, (299,299))
 
-        queries, query_vecs, references, reference_vecs = get_feature_only(intermediate_layer_model_big, queries, db, img_size=(333,333), lr=False)
-        gc.collect()
-        queries, query_vecs_lr, references, reference_vecs_lr = get_feature_only(intermediate_layer_model_small, queries,db, img_size=(224,224), lr=True)
-
-        query_vecs = np.concatenate([query_vecs,query_vecs_lr],axis=1)
-        reference_vecs = np.concatenate([reference_vecs, reference_vecs_lr],axis=1)
-
-        print('complete docom_feature  ',psutil.virtual_memory(), 'reference_vecs  shape', reference_vecs.shape)
+        # l2 normalization
         query_vecs = l2_normalize(query_vecs)
         reference_vecs = l2_normalize(reference_vecs)
 
@@ -111,128 +102,166 @@ def l2_normalize(v):
     return v / norm[:,None]
 
 
-def get_feature_only(intermediate_layer_model, queries, db, img_size=(299,299), lr=False, rmac_num=2, qe_i_num=1):
-    #    img_size = (224, 224)
+
+def extractHueHistogram(img, num_pixels):
+    #img = cv2.imread(path, cv2.IMREAD_COLOR)
+    #img = cv2.resize(path, size)
+    #img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hue = img[:,:,0] ###range: 0-180 (to fit range 0, 255)
+    histogram_hue = np.zeros(180, dtype=np.float)
+    histogram_hue = np.bincount(hue.ravel(), minlength=180).astype(np.float)
+    histogram_hue /= num_pixels
+    return histogram_hue
+
+import time
+
+"""
+def generator():
+    while 1:
+        for start in range(0, total, batch_size):
+            x_batch = []
+            end = min(start + batch_size, total)
+            y_batch = y_train[start:end, :]
+            file_names_batch = file_names_train[start:end]
+            for file_name in file_names_batch:
+                im = io.imread(os.path.join(train_jpg, file_name))
+                img = im.astype('float32') / 255
+                x_batch.append(img)
+            yield np.array(x_batch), y_batch
+"""
+
+#from skimage.transform import resize
+import skimage.transform as trans
+
+"""
+def testGenerator(test_path, num_image=30,target_size = (256,256),flag_multi_class = False,as_gray = True):
+    for i in range(num_image):
+        img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
+        img = img / 255
+        img = trans.resize(img,target_size)
+        img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
+        img = np.reshape(img,(1,)+img.shape)
+        yield img
+"""
+
+# data preprocess
+def get_feature(model, queries, db, img_size):
+#    img_size = (224, 224)
+
+
     batch_size = 200
-    # topResultsQE=5
-    L = rmac_num
-
+    #topResultsQE=5
+    L=3
     test_path = DATASET_PATH + '/test/test_data'
-    #intermediate_layer_model = Model(inputs=model.input,
-    #                                outputs=[model.get_layer('GAP_LAST').input, model.get_layer('GAP_LAST').output])
-    # test_datagen = ImageDataGenerator(preprocessing_function=aug_out_featurewise_val)
-    if lr ==True:
-        test_datagen = ImageDataGenerator(rescale=1. / 255, dtype='float32', samplewise_center=True,
-                                        samplewise_std_normalization=True)
-    else:
-        test_datagen = ImageDataGenerator(rescale=1. / 255, dtype='float32', samplewise_center=True,
-                                        samplewise_std_normalization=True, preprocessing_function = np.fliplr)
-
-    query_generator = test_datagen.flow_from_directory(
-        directory=test_path,
-        target_size=img_size,
-        classes=['query'],
-        color_mode="rgb",
+    intermediate_layer_model_small = Model(inputs=model.get_layer('input_1').input, outputs=[model.get_layer('GAP_LAST').input, model.get_layer('GAP_LAST').output])
+    intermediate_layer_model_big = Model(inputs=model.get_layer('input_1_2').input, outputs=[model.get_layer('GAP_LAST_2').input, model.get_layer('GAP_LAST_2').output])
+    #intermediate_layer_model = Model(inputs=model.input, outputs=model.get_layer('GAP_LAST').input)
+    
+    test_datagen = ImageDataGenerator(rescale=1./255.0, dtype='float32', samplewise_center=True, samplewise_std_normalization=True)
+    
+    """
+    test_generator = test_datagen.flow_from_directory(directory=test_path, target_size=(224, 224), classes=['query'], color_mode="rgb",
         batch_size=batch_size,
         class_mode=None,
-        shuffle=False
-    )
+        shuffle=False)
+    for t in range(len(test_generator)):
+        imgs = heuhist_generator.next()
+        for img in imgs:
+            img = img.reshape(hue_size[0], hue_size[1], 3)
+            img = img.astype(np.uint8)
+            query_hue_histogram.append(extractHueHistogram(img, hue_pixels))
+        yeild 
+    #test_generator2 = testGenerator(test_path)
+    """
 
-    query_vecs, gap_query_vecs = intermediate_layer_model.predict_generator(query_generator, steps=len(query_generator)*20,
-                                                                            workers=4)
-    print('complete predict query_vecs  ',psutil.virtual_memory(), 'quer vec  shape', query_vecs.shape)
-    reference_generator = test_datagen.flow_from_directory(
-        directory=test_path,
-        target_size=img_size,
-        classes=['reference'],
-        color_mode="rgb",
+
+    
+    test_generator_small = test_datagen.flow_from_directory(directory=test_path, target_size=(224, 224), classes=['query'], color_mode="rgb",
         batch_size=batch_size,
         class_mode=None,
-        shuffle=False
-    )
+        shuffle=False)
+    
+    test_generator_big = test_datagen.flow_from_directory(directory=test_path, target_size=(299, 299), classes=['query'], color_mode="rgb",
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    
+    query_vecs_small, gap_query_vecs_small = intermediate_layer_model_small.predict_generator(test_generator_small, steps=len(test_generator_small), workers=8)
+    query_vecs_big, gap_query_big = intermediate_layer_model_big.predict_generator(test_generator_big, steps=len(test_generator_big), workers=8)
+    print(query_vecs_small.shape) ######## 8*8*1536 , 6*6*1536 정도로 나올수 있음. 
+    print(gap_query_vecs_small.shape)
+    print(query_vecs_big.shape)
+    print(gap_query_big.shape)
 
-    reference_vecs, gap_reference_vecs = intermediate_layer_model.predict_generator(reference_generator,
-                                                                                    steps=len(reference_generator)*10,
-                                                                                    workers=4)
-    print('complete predict reference_vecs  ',psutil.virtual_memory(), 'reference_vecs  shape', query_vecs.shape)
+    """
+    test_generator_small = test_datagen.flow_from_directory(directory=test_path, target_size=(224, 224), classes=['reference'], color_mode="rgb",
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    test_generator_big = test_datagen.flow_from_directory(directory=test_path, target_size=(299, 299), classes=['reference'], color_mode="rgb",
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    
+    ref_vecs_small, gap_ref_vecs_small = intermediate_layer_model_small.predict_generator([test_generator_small, test_generator_big], steps=len(test_generator_big), workers=8)
+    ref_vecs_big, gap_ref_vecs_big = intermediate_layer_model_big.predict_generator([test_generator_small, test_generator_big], steps=len(test_generator_big), workers=8)
+    
+    print(ref_vecs_small.shape)
+    print(gap_ref_vecs_small.shape)
+    print(ref_vecs_big.shape)
+    print(gap_ref_vecs_big.shape)
+    """
+    queries, queryMAC, db, DbMAC = []
+    """
+
+    #reference_vecs = intermediate_layer_model.predict_generator(reference_generator, steps=len(reference_generator),workers=4)
 
     # ------------------ DB images: reading, descripting and whitening -----------------------
-    DbMAC = extractRMAC(reference_vecs, True, L)
-    #DbMAC = np.array(DbMAC)
-    DbMAC_sumpool = sumPooling(DbMAC, reference_vecs.shape[0], False)
-    print('DbMAC_sumpool lenght',len(DbMAC_sumpool),DbMAC_sumpool[0].shape,psutil.virtual_memory() )
+    t1 = time.clock()
+    DbMAC = extractRMAC(reference_vecs, intermediate_layer_model, True, L)
+    ## vector PCA
+    #print('start pca_model_loader')
+    #W, Xm = learningPCA(DbMAC)
+    #print('W shape:', W.shape, 'Xm shape:',Xm.shape)
+
+    #print("PCA-whitening")
+    #DbMAC = apply_whitening(DbMAC, Xm, W)
+    DbMAC = np.array(DbMAC)
+    DbMAC = sumPooling(DbMAC, reference_vecs.shape[0], False)
+    Dbtime = time.clock() - t1
+    print('DbMAC lenght',len(DbMAC))
+    print("RMAC and PCA-whitening of terminated in",round(Dbtime),"s")
+
     # ------------------- query images: reading, descripting and whitening -----------------------
-    queryMAC = extractRMAC(query_vecs, True, L)
-    #queryMAC = np.array(queryMAC)
-    queryMAC_sumpool = sumPooling(queryMAC, query_vecs.shape[0], False)
-    print('queryMAC_sumpool lenght',len(queryMAC_sumpool),queryMAC_sumpool[0].shape,psutil.virtual_memory() )
+    t1 = time.clock()
+    queryMAC = extractRMAC(query_vecs, intermediate_layer_model, True, L)
+    #print("PCA-whitening")
+    #queryMAC = apply_whitening(queryMAC, Xm, W)
+    queryMAC = np.array(queryMAC)
+    queryMAC = sumPooling(queryMAC, query_vecs.shape[0], False)
+    print('queryMAC lenght',len(queryMAC))
+    Dbtime = time.clock() - t1
+    print("Query descriptors saved!")
+    print("queryMAC and PCA-whitening of terminated in",round(Dbtime),"s")
 
+    queryMAC = np.array(queryMAC)
+    DbMAC = np.array(DbMAC)
+    queryMAC = queryMAC.squeeze()
+    DbMAC = DbMAC.squeeze()
 
+    #retrieval1 = time.clock()
+    #finalReRank = retrieveQENEW(queryMAC, regions, topResultsQE,url, queryImages, DbImages, dataset)
+    #retrieval2 = time.clock() - retrieval1
+    #print("AVG query time:",round(retrieval2/len(queryImages),2),"s")
 
-    DbMAC_sumpool = np.array(DbMAC_sumpool)
-    DbMAC_sumpool = DbMAC_sumpool.squeeze()
-    print('DbMAC_sumpool shape',DbMAC_sumpool.shape,psutil.virtual_memory() )
-    queryMAC_sumpool = np.array(queryMAC_sumpool)
-    queryMAC_sumpool = queryMAC_sumpool.squeeze()
-    print('queryMAC_sumpool shape',queryMAC_sumpool.shape,psutil.virtual_memory() )
-
-    gap_query_vecs = l2_normalize(gap_query_vecs)
-    gap_reference_vecs = l2_normalize(gap_reference_vecs)
-
-    query_vecs = np.concatenate([queryMAC_sumpool,gap_query_vecs],axis=1)
-    reference_vecs = np.concatenate([DbMAC_sumpool, gap_reference_vecs],axis=1)
-    print('complete concate gap sum, query_vecs shape',query_vecs.shape,psutil.virtual_memory() )
-    # l2 normalization
-    query_vecs = l2_normalize(query_vecs)
-    reference_vecs = l2_normalize(reference_vecs)
-    # pca
-    all_vecs = np.concatenate([query_vecs, reference_vecs])
-    all_pca_vecs = PCA(1024).fit_transform(all_vecs)
-    query_vecs = all_pca_vecs[:query_vecs.shape[0],:]
-    reference_vecs = all_pca_vecs[query_vecs.shape[0]:,:]
-
-    # l2 normalization
-    query_vecs = l2_normalize(query_vecs)
-    reference_vecs = l2_normalize(reference_vecs)
-    print('complete pca, query_vecs shape',query_vecs.shape,psutil.virtual_memory() )
-    # Calculate cosine similarity for QE
-    qe_iter = 2
-    qe_number = 19
-    dba_number = 9
-    for i in range(qe_iter):
-        qe_number = qe_number // (i+1)
-        dba_number = dba_number // (i+1)
-        weights = np.logspace(0, -1.5, (qe_number+1))
-        weights /= weights.sum()
-        pre_sim_matrix = np.dot(query_vecs, reference_vecs.T)
-        pre_indices = np.argsort(pre_sim_matrix, axis=1) #lower first
-        pre_indices = np.flip(pre_indices, axis=1) #higher first
-        for i in range(query_vecs.shape[0]):
-            query_vecs[i] *= weights[0]
-            for refidx in range(qe_number):
-                query_vecs[i] += reference_vecs[pre_indices[i][refidx]]*weights[refidx+1]
-
-        # after query expanstion l2 normalization
-        query_vecs = l2_normalize(query_vecs)
-
-        # Calculate cosine similarity for DBA
-        weights = np.logspace(0, -1.5, (dba_number+1))
-        weights /= weights.sum()
-        pre_sim_matrix = np.dot(reference_vecs, query_vecs.T)
-        pre_indices = np.argsort(pre_sim_matrix, axis=1) #lower first
-        pre_indices = np.flip(pre_indices, axis=1) #higher first
-        for i in range(reference_vecs.shape[0]):
-            reference_vecs[i] *= weights[0]
-            for refidx in range(dba_number):
-                reference_vecs[i] += query_vecs[pre_indices[i][refidx]]*weights[refidx+1]
-
-        # after database augment l2 normalization
-        reference_vecs = l2_normalize(reference_vecs)
-    print('query expansion complete',query_vecs.shape,psutil.virtual_memory() )
-
-
-    return queries, query_vecs, db, reference_vecs
-
+    #retrieval1 = time.clock()
+    #finalReRank2 = retrieveQERegionsNEW(queryMAC, regions, topResultsQE, url,queryImages, DbImages, finalReRank, dataset)
+    #retrieval2 = time.clock() - retrieval1
+    #print("AVG query expansion time:",round(retrieval2/len(queryImages),2),"s")
+    """
+    return queries, queryMAC, db, DbMAC
+    
 def build_model(backbone= None, input_shape =  (224,224,3), use_imagenet = 'imagenet', num_classes=1383, base_freeze=True, opt = SGD(), NUM_GPU=1,use_gap_net=False):
     base_model = backbone(input_shape=input_shape, weights=use_imagenet, include_top= False)#, classes=NCATS)
     x = base_model.output
@@ -403,39 +432,37 @@ if __name__ == '__main__':
     nb_epoch = config.epoch
     batch_size = config.batch_size
     num_classes = config.num_classes
-    input_shape_large = (333,333,3)
-    input_shape_small = (224, 224, 3)  # input image shape
+    input_shape = (299,299,3)#(224, 224, 3)  # input image shape
     use_gap_net = False
     opt = keras.optimizers.Adam(lr=0.0005)
-
+    #model = build_model(backbone= InceptionResNetV2, input_shape = input_shape, use_imagenet = None, num_classes=num_classes, base_freeze=True, opt =opt,use_gap_net=use_gap_net)
+    #bind_model(model)
 
     small_model = build_model(backbone= InceptionResNetV2, input_shape = (224, 224, 3), use_imagenet = None, num_classes=num_classes, base_freeze=True, opt =opt,use_gap_net=use_gap_net)
+    #intermediate_layer_model = Model(inputs=model.input, outputs=model.get_layer('GAP_LAST').input)
     x = small_model.output
     fake_out_small = Dense(num_classes, activation='softmax', name='last_softmax1')(x)
-    bind_model(small_model)
-    nsml.load(checkpoint='secls_637_31', session='Zonber/ir_ph2/466')#224
+    
+    #bind_model(small_model)
+    #nsml.load(checkpoint='secls_637_19', session='Zonber/ir_ph2/466') #InceptionResnetV2 222
     small_intermediate_layer_model = Model(inputs=small_model.input, outputs=fake_out_small)
 
-    large_model = build_model(backbone= InceptionResNetV2, input_shape = input_shape_large, use_imagenet = None, num_classes=num_classes, base_freeze=True, opt =opt,use_gap_net=use_gap_net)
+    #small_intermediate_layer_model = Model(inputs=small_model.input, outputs=[small_model.get_layer('GAP_LAST').input, small_model.get_layer('GAP_LAST').output])
+    #small_intermediate_layer_model.add
+    
+    large_model = build_model(backbone= InceptionResNetV2, input_shape = (299, 299, 3), use_imagenet = None, num_classes=num_classes, base_freeze=True, opt =opt,use_gap_net=use_gap_net)
     x = large_model.output
     for i, layer in enumerate(large_model.layers):
         layer.name += '_2'
+
     fake_out_big = Dense(num_classes, activation='softmax', name='last_softmax2')(x)
-    bind_model(large_model)
-    nsml.load(checkpoint='secls_222_20', session='Zonber/ir_ph2/450')#333
+    #bind_model(large_model)
+    #nsml.load(checkpoint='secls_222_33', session='Zonber/ir_ph2/314')
     large_intermediate_layer_model = Model(inputs=large_model.input, outputs=fake_out_big)#[large_model.get_layer('GAP_LAST').input, large_model.get_layer('GAP_LAST').output])
 
     finalOut = keras.layers.concatenate([small_intermediate_layer_model.output,large_intermediate_layer_model.output], axis=1) #Concatenate(axis=1)([small_intermediate_layer_model.output,large_intermediate_layer_model.output])
     joinedModel = Model([small_intermediate_layer_model.input, large_intermediate_layer_model.input], finalOut)
     bind_model(joinedModel)
-    
-
-    #model = build_model(backbone= InceptionResNetV2, input_shape = input_shape_large, use_imagenet = None, num_classes=num_classes, base_freeze=True, opt =opt,use_gap_net=use_gap_net)
-    #bind_model(model)
-
-    #nsml.load(checkpoint='secls_222_33', session='Zonber/ir_ph2/314') #InceptionResnetV2 222
-    #nsml.save('large')  # this is display model name at lb
-
 
     if config.pause:
         nsml.paused(scope=locals())
@@ -443,12 +470,9 @@ if __name__ == '__main__':
     bTrainmode = False
     if config.mode == 'train':
         bTrainmode = True
-    
-        
-    #nsml.load(checkpoint='secls_222_20', session='Zonber/ir_ph2/450')#333
-    #nsml.load(checkpoint='secls_637_31', session='Zonber/ir_ph2/466')#224
-    #nsml.load(checkpoint='secls_222_33', session='Zonber/ir_ph2/314')#299
-    nsml.save('over_over_fitting')
-    #for i in range(14,23):
-    #    nsml.load(checkpoint='secls_222_'+str(i), session='Zonber/ir_ph2/450')
-    #    nsml.save('secls_222_'+str(i))
+
+    nsml.load(checkpoint='over_over_fitting_comb', session='Zonber/ir_ph2/618') 
+    nsml.save('over_over_fitting_comb')
+
+    #nsml.load(checkpoint='secls_222_27', session='Zonber/ir_ph2/314') #InceptionResnetV2 222
+    #nsml.save('over_over_fitting')  # this is display model name at lb
